f326f8f62e84deddc9612ce5f4f47b1bfa0e4434
kevin1920 <56529861+kevin1920@users.noreply.github.com>
2021-04-21 15:00:25 -0500

    Se hizo el cambio de la key

--- software4 api/server/keys.js
+++ software4 api/server/keys.js
@@ -1,6 +1,6 @@
 process.env.USERDB = "postgres"
 process.env.HOSTDB = "localhost"
 process.env.DB = "mvpSoftware3"
-process.env.PASSWORD = "alexyaleja"
+process.env.PASSWORD = ""
 process.env.PORT_DB = 5432
 process.env.SECRET_KEY = "0fabd9e303112843914fba9eed47d6b5"
